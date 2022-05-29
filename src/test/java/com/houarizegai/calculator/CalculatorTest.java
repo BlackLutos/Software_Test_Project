bfe8cc1a764270538e5e986327ed39e32e37ed81
BlackLutos <gcobs172250@gmail.com>
2022-05-30 03:22:12 +0800

    Add files via upload

--- src/test/java/com/houarizegai/calculator/CalculatorTest.java
+++ src/test/java/com/houarizegai/calculator/CalculatorTest.java
@@ -16,23 +16,23 @@ class CalculatorTest {
     /*
      * testCalc() test method
      */
-    @Test
-    void testCalc() {
-        double first = 3;
-        String second = "5";
-
-        try {
-            Assertions.assertEquals(8, calculator.calc(first, second, '+'));
-            Assertions.assertEquals(-2, calculator.calc(first, second, '-'));
-            Assertions.assertEquals(15, calculator.calc(first, second, '*'));
-            Assertions.assertEquals(0.6, calculator.calc(first, second, '/'));
-            Assertions.assertEquals(3d, calculator.calc(first, second, '%'));
-            Assertions.assertEquals(243, calculator.calc(first, second, '^'));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-    }
+//    @Test
+//    void testCalc() {
+//        double first = 3;
+//        String second = "5";
+//
+//        try {
+//            Assertions.assertEquals(8, calculator.calc(first, second, '+'));
+//            Assertions.assertEquals(-2, calculator.calc(first, second, '-'));
+//            Assertions.assertEquals(15, calculator.calc(first, second, '*'));
+//            Assertions.assertEquals(0.6, calculator.calc(first, second, '/'));
+//            Assertions.assertEquals(3d, calculator.calc(first, second, '%'));
+//            Assertions.assertEquals(243, calculator.calc(first, second, '^'));
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//
+//    }
     @Test
     void testCalc_plus(){
         double first = 200;
